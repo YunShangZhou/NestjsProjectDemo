80e0d17c05a3f3d1536c557760d020f21bb8ef1b
Chen Kaibang <yunshangzhou@163.com>
2024-01-21 09:06:51 +0800

    chore: 修改mysql连接端口

--- src/app.module.ts
+++ src/app.module.ts
@@ -54,7 +54,7 @@ const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
     // TypeOrmModule.forRoot({
     //   type: 'mysql',
     //   host: 'localhost',
-    //   port: 3090,
+    //   port: 3306,
     //   username: 'root',
     //   password: '123456',
     //   database: 'testdb',
